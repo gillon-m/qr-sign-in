a544306b4a45f63d8f618a34f4c5fd89df9813a0
gillon-m <gillon.m@outlook.com>
2023-04-05 13:27:13 +1000

    initialise springboot

---
+++ src/main/java/com/manalastas/qrsignin/QrSignInApplication.java
@@ -0,0 +1,13 @@
+package com.manalastas.qrsignin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrSignInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrSignInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manalastas/qrsignin/QrSignInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manalastas.qrsignin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrSignInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
